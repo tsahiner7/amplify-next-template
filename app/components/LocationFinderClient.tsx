8196312df9fabb1fc3a216878a9521bb894341a8
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-04-03 10:37:33 -0500

    weatherInfo fetched and displayed on Server and Client side...

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -1,27 +1,49 @@
 'use client';
-import { getLocationOrigin } from "next/dist/shared/lib/utils";
 import { useEffect, useState } from "react";
 
 export default function LocationFinderServer() {
 
-    const [locationInfo, setLocationInfo] = useState({City: 'N/A'});
+    const [locationInfo, setLocationInfo] = useState({City: 'N/A', Longitude: 'N/A', Latitude: 'N/A'});
+
+    const [weatherInfo, setWeatherInfo] = useState({dataseries: []});
 
     const getLocationInfo = async () => {
         const response = await fetch('https://apip.cc/json');
         const locationData = await response.json();
-        console.log(locationData);
+        // console.log(locationData);
         setLocationInfo(locationData);
+    }
+
+    const getWeatherInfo = async (lon:any, lat:any) => {
+        const response = await fetch(`https://www.7timer.info/bin/astro.php?lon=${lon}&lat=${lat}&ac=0&unit=metric&output=json&tzshift=0`);
+        const weatherData = await response.json();
+        // console.log(weatherData);
+        setWeatherInfo(weatherData);
     }
 
 
     useEffect(() => {
         getLocationInfo();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const { Longitude, Latitude } = locationInfo;
+        if (Longitude !== 'N/A' && Latitude !== 'N/A') {
+            getWeatherInfo(Longitude, Latitude);
+        }
+    }, [locationInfo]);
 
 
     return (
         <>
         <h1>Hello from {locationInfo.City} - Client Component</h1>
+        <p>
+            {
+                weatherInfo.dataseries.length > 0
+                ? `It is currently ${weatherInfo.dataseries[0].temp2m} degrees celsius`
+                : 'Loading weather...'
+            }
+        </p>
         </>
     )
 }
--- app/components/LocationFinderServer.tsx
+++ app/components/LocationFinderServer.tsx
@@ -3,12 +3,22 @@ export default async function LocationFinderServer() {
     const response = await fetch('https://apip.cc/json');
     const locationData = await response.json();
     console.log(locationData);
-    console.log("Hello");
     const locationInfo = locationData;
+    
+    const lon = locationInfo.Longitude;
+    const lat = locationInfo.Latitude;
+
+    //Get current weather info from location according to lon and lat
+    const responseWeather = await fetch(`https://www.7timer.info/bin/astro.php?lon=${lon}&lat=${lat}&ac=0&unit=metric&output=json&tzshift=0`);
+    const weatherData = await responseWeather.json();
+    console.log(weatherData.dataseries[0].temp2m);
+    const weatherInfo = weatherData;
+
 
     return (
         <>
         <h1>Hello from {locationInfo?.City} - Server Component</h1>
+        <p>It is currently {weatherInfo?.dataseries[0].temp2m} degrees celsius</p>
         </>
     )
 }
