dbdd8410e69c190c51005151f51a08f990cca65f
Tolga Sahiner <tolgasahiner02@gmail.com>
2025-04-07 12:50:01 -0500

    Imported ContentLoader, improved code...

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -1,11 +1,12 @@
 'use client';
 import { useEffect, useState } from "react";
+import ContentLoader from "react-content-loader";
 
 export default function LocationFinderServer() {
 
-    const [locationInfo, setLocationInfo] = useState({City: 'N/A', Longitude: 'N/A', Latitude: 'N/A'});
+    const [locationInfo, setLocationInfo] = useState({City: "Loading...", Longitude: 'N/A', Latitude: 'N/A'});
 
-    const [weatherInfo, setWeatherInfo] = useState({dataseries: []});
+    const [weatherInfo, setWeatherInfo] = useState<any>(null);
 
     const getLocationInfo = async () => {
         const response = await fetch('https://apip.cc/json');
@@ -34,16 +35,42 @@ export default function LocationFinderServer() {
     }, [locationInfo]);
 
 
+    const MyLoader = () => (
+        <ContentLoader 
+          speed={2}
+          width={400}
+          height={160}
+          viewBox="0 0 400 160"
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="48" y="8" rx="3" ry="3" width="88" height="6" /> 
+          <rect x="48" y="26" rx="3" ry="3" width="52" height="6" /> 
+          <rect x="0" y="56" rx="3" ry="3" width="410" height="6" /> 
+          <rect x="0" y="72" rx="3" ry="3" width="380" height="6" /> 
+          <rect x="0" y="88" rx="3" ry="3" width="178" height="6" /> 
+          <circle cx="20" cy="20" r="20" />
+        </ContentLoader>
+      )
+
+    const isLoading = locationInfo.City === "Loading..." || !weatherInfo;
+
     return (
         <>
-        <h1>Hello from {locationInfo.City} - Client Component</h1>
-        <p>
             {
-                weatherInfo.dataseries.length > 0
-                ? `It is currently ${weatherInfo.dataseries[0].temp2m} degrees celsius`
-                : 'Loading weather...'
+                isLoading 
+                ? (
+                    <MyLoader />
+                ) 
+                : (
+                    <>
+                        <h1>Hello from {locationInfo.City} - Client Component</h1>
+                        <p>
+                            It is currently {weatherInfo.dataseries[0].temp2m} degrees celsius
+                        </p>
+                    </>
+                )
             }
-        </p>
         </>
     )
 }
--- app/components/LocationFinderServer.tsx
+++ app/components/LocationFinderServer.tsx
@@ -2,23 +2,22 @@ export default async function LocationFinderServer() {
 
     const response = await fetch('https://apip.cc/json');
     const locationData = await response.json();
-    console.log(locationData);
-    const locationInfo = locationData;
+    // console.log(locationData);
     
-    const lon = locationInfo.Longitude;
-    const lat = locationInfo.Latitude;
+    let lon = locationData.Longitude;
+    let lat = locationData.Latitude;
 
     //Get current weather info from location according to lon and lat
     const responseWeather = await fetch(`https://www.7timer.info/bin/astro.php?lon=${lon}&lat=${lat}&ac=0&unit=metric&output=json&tzshift=0`);
     const weatherData = await responseWeather.json();
-    console.log(weatherData.dataseries[0].temp2m);
-    const weatherInfo = weatherData;
+    console.log(locationData);
+    console.log(lat, lon);
 
 
     return (
         <>
-        <h1>Hello from {locationInfo?.City} - Server Component</h1>
-        <p>It is currently {weatherInfo?.dataseries[0].temp2m} degrees celsius</p>
+        <h1>Hello from {locationData?.City} - Server Component</h1>
+        <p>It is currently {weatherData?.dataseries[0].temp2m} degrees celsius</p>
         </>
     )
 }
